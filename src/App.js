fc3fb3d1d9ebdb2fa361c91b502528bfcfde4a38
Eva Dutra <evinhaleao@gmail.com>
2022-02-03 16:20:38 +0100

    get data from mock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/search-bar';
 import NewsList from './components/news-list';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+      Axios.get('http://localhost:3000/hits')
+        .then(resp => {
+        const data = resp.data;
+        console.log(data);
+         })
+        .catch(error => {
+        console.log(error);
+    })}, [] 
+    );
+
+          
   return (
     <div className="App">
       <header className="App-header">
@@ -14,4 +27,6 @@ function App() {
   );
 }
 
-export default App;
+
+
+export default App
